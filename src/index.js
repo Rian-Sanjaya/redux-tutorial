f379cf00e459e264dbfe617ef51d379c0d3fba11
Rian Sanjaya <rian.sj@gmail.com>
2019-03-30 23:03:42 +0700

    redux file organization

--- src/index.js
+++ src/index.js
@@ -4,7 +4,31 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// create store
+import { combineReducers, createStore } from 'redux'
+// Provider to give access the app to the store
+import { Provider } from 'react-redux'
+import productsReducer from './reducers/products-reducer'
+import userReducer from './reducers/user-reducer'
+
+const allReducers = combineReducers({
+    products: productsReducer,
+    user: userReducer
+});
+
+// need to pass reducer into createStore
+// to pre-populate the store, we pass in the initial state as the sedond argument
+const store = createStore(
+    allReducers, 
+    {
+        products: [{name: 'iPhone'}],
+        user: 'Micheal'
+    },
+    window.devToolsExtension && window.devToolsExtension()
+);
+
+// Provider give the app access to the store
+ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
